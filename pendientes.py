40544fda8a2684a52e6580f9569568aa6eb3e392
coderchemivan <igomezs1502@comunidad.unam.mx>
2022-07-08 15:47:40 -0500

    "See what list type is better"

--- pendientes.py
+++ pendientes.py
@@ -1,76 +1,185 @@
-from kivy.lang import Builder
 
-from kivymd.app import MDApp
-from kivymd.uix.button import MDFlatButton
 from kivymd.uix.dialog import MDDialog
+from kivy.uix.popup import Popup
+from kivymd.uix.button import MDRaisedButton, MDFlatButton
+from kivy.lang import Builder
+from kivymd.app import MDApp
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivymd.uix.list import TwoLineListItem
-from kivymd.uix.list import OneLineAvatarIconListItem
+from kivymd.uix.snackbar import Snackbar
+from kivy.properties import ObjectProperty
 
-from kivymd.uix.list import OneLineAvatarIconListItem
-from kivymd.uix.list import OneLineListItem
+KV='''
+
+<DialogContent>
+    confirm_check_in_list: confirm_check_in_list
+    id: confirm_check_in_dialog
+    cols: 1
+    spacing: dp(20)
+    size_hint_y: None
+    size_hint_x: None
+    height: self.height
+    width: self.width
+
+    AnchorLayout:
+
+        ScrollView:
+
+            MDList:
+                id: confirm_check_in_list
 
 
-KV = '''
-<ItemConfirm>
-    on_release: root.set_icon(check)
 
 
-    CheckboxLeftWidget:
-        id: check
-        group: "check"
 
-
-MDFloatLayout:
-
-    MDFlatButton:
-        text: "ALERT DIALOG"
-        pos_hint: {'center_x': .5, 'center_y': .5}
-        on_release: app.show_confirmation_dialog()
 '''
 
+class DialogContent(BoxLayout):
+    confirm_check_in_list=ObjectProperty()
+    def check_conflicts(self, materias):
+        c = 0
+        for materia in materias[0]:
+            self.ids.confirm_check_in_list.add_widget(
+                TwoLineListItem(text=f'{materia}',
+                    secondary_text=f'{materias[1][c]}',
 
-
-class ItemConfirm(OneLineAvatarIconListItem):
-    divider = None
-
-    def set_icon(self, instance_check):
-        instance_check.active = True
-        check_list = instance_check.get_widgets(instance_check.group)
-        for check in check_list:
-            if check != instance_check:
-                check.active = False
-        print("este es una prueba")
-
+                )
+            )
+            c+=1
 
 class pendientes_list():
-    dialog = None
+
+    dialog=None
 
     def build(self):
+
         return Builder.load_string(KV)
+    #
+    # def close_dialog(self, obj):
+    #     self.dialog.dismiss()
+    #
+    # def confirm_selection(self, obj):
+    #     #check number in quantity field
+    #
+    #     Snackbar(text='Success').open()
 
     def show_dialog(self,materias_act):
-
         if not self.dialog:
             self.dialog = MDDialog(
-                title="Phone ringtone",
-                type="confirmation",
-                items=[ItemConfirm(text=str(materia)) for materia in materias_act[0]
-                ],
-                buttons=[
-                    MDFlatButton(
-                        text="CANCEL",
-                        theme_text_color="Custom",
-
-                    ),
-                    MDFlatButton(
-                        text="OK",
-                        theme_text_color="Custom",
-
-                    ),
-                ],
+                title='Check products',
+                type='custom',
+                content_cls=DialogContent(),
+                size_hint=(1,1),
             )
+
+        self.dialog.content_cls.check_conflicts(materias_act)
         self.dialog.open()
+        #         self.dialog = Popup(
+        #             title='Check products',
+        #             size_hint=(.7,.6),
+        #             content=DialogContent())
+        # self.dialog.content.check_conflicts(materias_act)
+        # self.dialog.open()
 
 
 
-#hola
+
+
+
+
+
+
+
+
+
+
+
+
+# from kivymd.uix.dialog import MDDialog
+# from kivy.uix.popup import Popup
+# from kivymd.uix.button import MDRaisedButton, MDFlatButton
+# from kivy.lang import Builder
+# from kivymd.app import MDApp
+# from kivy.uix.screenmanager import Screen, ScreenManager
+# from kivy.uix.gridlayout import GridLayout
+# from kivy.uix.boxlayout import BoxLayout
+# from kivy.uix.floatlayout import FloatLayout
+# from kivymd.uix.list import TwoLineListItem
+# from kivymd.uix.snackbar import Snackbar
+# from kivy.properties import ObjectProperty
+#
+# KV='''
+#
+# <DialogContent>
+#
+#
+#     confirm_check_in_list: confirm_check_in_list
+#     id: confirm_check_in_dialog
+#     size_hint_y: None
+#     size_hint_x: None
+#     height: self.height
+#     width: self.width
+#
+#     Button:
+#         text: "hola"
+#         pos_hint:{"x":0.1,"y":0}
+#         size_hint:0.8,0.2
+#
+#     MDList:
+#         id: confirm_check_in_list
+#         pos_hint:{"x":0.1,"y":0.7}
+#         size_hint:0.8,0.8
+#
+# '''
+#
+#
+#
+#
+# class DialogContent(FloatLayout):
+#     confirm_check_in_list=ObjectProperty()
+#     def check_conflicts(self, materias):
+#         c = 0
+#         for materia in materias[0]:
+#             self.ids.confirm_check_in_list.add_widget(
+#                 TwoLineListItem(text=f'{materia}',
+#                     secondary_text=f'{materias[1][c]}',
+#
+#                 )
+#             )
+#             c+=1
+#
+# class pendientes_list():
+#
+#     dialog=None
+#
+#     def build(self):
+#
+#         return Builder.load_string(KV)
+#     #
+#     # def close_dialog(self, obj):
+#     #     self.dialog.dismiss()
+#     #
+#     # def confirm_selection(self, obj):
+#     #     #check number in quantity field
+#     #
+#     #     Snackbar(text='Success').open()
+#
+#     def show_dialog(self,materias_act):
+#         if not self.dialog:
+#         #     self.dialog = MDDialog(
+#         #         type='custom',
+#         #         title= 'This is a very long long long text',
+#         #         size_hint=(1, 1),
+#         #         content_cls=DialogContent(),
+#         #     )
+#
+#         # self.dialog.content_cls.check_conflicts(materias_act)
+#         # self.dialog.open()
+#                 self.dialog = Popup(
+#                     title='Check products',
+#                     size_hint=(.7,.6),
+#                     content=DialogContent())
+#         self.dialog.content.check_conflicts(materias_act)
+#         self.dialog.open()
