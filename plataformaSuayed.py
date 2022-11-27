646fc32e81ded87fe2b6c152d0511ffcc0eceaf2
coderchemivan <igomezs1502@comunidad.unam.mx>
2022-11-27 12:52:52 -0600

    Add transformData class

--- plataformaSuayed.py
+++ plataformaSuayed.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 import mysql.connector
-
+import datetime
 
 
 
@@ -88,8 +88,8 @@ class Feedback():
                 status_calificacion = cur.fetchall()
                 status_calificacion, = list(zip(*status_calificacion))
                 status_calificacion = status_calificacion[0]
-
-                if status_calificacion == 1:
+                
+                if status_calificacion == 3:
                     continue
 
                 # Abriendo la actividad
@@ -97,77 +97,131 @@ class Feedback():
                     actividad = self.driver.find_element(By.XPATH,
                                                          './/span[contains(text(),"' + activity + '")]').click()
                 except:
-                    continue
+                    try:
+                        actividad = self.driver.find_element(By.XPATH,
+                                                            './/span[contains(text(),"' + activity.replace('/','//') + '")]').click()
+                    except:
+                        print(activity)
+                        continue
 
 
-                # Obteniendo calificación y fecha de calificación de la actividad
-                calificacion = ""
-                calificado_en= ""
                 try:
-                    feedback_content = self.driver.find_elements_by_xpath('.//td[@class="cell c1 lastcol"]')
-                    calificado_en = feedback_content[5].text
-                    calificacion = feedback_content[4].text.split("/")[0].strip()
+                    #status de la entrega
+                    fecha_entregada = None
+                    entrega_status_contendor = self.driver.find_elements(By.XPATH, "//*[@class='submissionstatustable']//*[@class='cell c1 lastcol']")
+                    index = 0
+                    for status in entrega_status_contendor:
+                        if entrega_status_contendor[index].text.startswith("Monday") or entrega_status_contendor[index].text.startswith("Tuesday") or entrega_status_contendor[index].text.startswith("Wednesday") or entrega_status_contendor[index].text.startswith("Thursday") or entrega_status_contendor[index].text.startswith("Friday") or entrega_status_contendor[index].text.startswith("Saturday") or entrega_status_contendor[index].text.startswith("Sunday"):
+                            fecha_entregada = entrega_status_contendor[index].text
+                            break
+                        index += 1
+                    fecha_entregada = tranformData.transform_date(fecha_entregada)         
+                    fecha_entrega = cur.execute("SELECT fecha_entrega FROM actividades WHERE clave_materia ="+materia+" AND name = '" + activity + "'")
+                    fecha_entrega = cur.fetchall()
+                    fecha_entrega, = list(zip(*fecha_entrega))
+                    fecha_entrega = fecha_entrega[0]
+                    
+                    if fecha_entregada != None:
+                        update_fecha_entregada = "UPDATE actividades SET entregada_el = '" + fecha_entregada + \
+                                                    "' WHERE clave_materia = " + materia + " AND name = '" + activity + "'" 
+                        #transformando fecha_entregada de str a datetime
+                        fecha_entregada = datetime.datetime.strptime(fecha_entregada, '%Y-%m-%d')
+                    if fecha_entregada!= None and fecha_entregada.date() > fecha_entrega:
+                        update_status = 'Entregada con atraso'
+                        update_act_status = "UPDATE actividades SET status =" + '"'+update_status+ '"'+ "WHERE clave_materia = " + materia + " AND name = '" + activity + "'"    
+                    elif fecha_entregada!= None and fecha_entregada.date() <= fecha_entrega:
+                        update_status = 'Entregada a tiempo'
+                        update_act_status = "UPDATE actividades SET status =" + '"'+update_status+ '"'+ "WHERE clave_materia = " + materia + " AND name = '" + activity + "'"  
+                        cur.execute(update_act_status)  
+                    
 
-                    update_fecha_calificacion = "UPDATE actividades SET calificada_en = '" + calificado_en + \
-                                                "' WHERE clave_materia ="+materia+" AND name = '" + activity + "'"
+                    #feedback
+                    feedback_content = self.driver.find_elements(By.XPATH, './/div[@class="feedback"]//td')   
+                    calificacion = ""
+                    calificado_en= ""
+                    if len(feedback_content) > 0:
+                        calificacion = feedback_content[0].text.split("/")[0].strip()
+                        calificado_en = feedback_content[1].text
+                        calificado_en =tranformData.transform_date(calificado_en)
+                    
+                        update_fecha_calificacion = "UPDATE actividades SET calificada_en = '" + calificado_en + \
+                                                    "' WHERE clave_materia ="+materia+" AND name = '" + activity + "'"
 
-                    update_calificacion = "UPDATE actividades SET calificacion =" + calificacion +  \
-                                          " WHERE clave_materia ="+materia+" AND name = '" + activity + "'"
+                        update_calificacion = "UPDATE actividades SET calificacion =" + calificacion +  \
+                                                " WHERE clave_materia ="+materia+" AND name = '" + activity + "'"
 
-                    update_calificacion_status = "UPDATE actividades SET cal_status = 1 " \
-                                                 "WHERE clave_materia =" + materia + " AND name = '" + activity + "'"
+                        update_calificacion_status = "UPDATE actividades SET cal_status = 1 " \
+                                                        "WHERE clave_materia =" + materia + " AND name = '" + activity + "'"
+                        cur.execute(update_calificacion_status)
+                        cur.execute(update_fecha_calificacion)
+                        cur.execute(update_calificacion)
 
+                    cur.execute(update_fecha_entregada) if fecha_entregada != None else None
 
-                    cur.execute(update_calificacion_status)
-                    cur.execute(update_fecha_calificacion)
-                    cur.execute(update_calificacion)
 
                     actividades_materia = cur.fetchall()
-                except:
-                    pass
+
 
                 # Obteniendo comentarios de la actividad
-                feedback = ""
-                try:
-                    btn_show_more = self.driver.find_element(By.XPATH,
-                                                             '//*[contains(@class,"expandsummaryicon expand")]').click()  # btn_show_more = driver.find_element(By.XPATH,'//*[@class="expandsummaryicon expand_assignfeedback_comments_386697"]')
-                    comentarios = self.driver.find_elements_by_xpath(
-                        '//*[contains(@class,"box py-3 boxaligncenter full_assignfeedback_comments")]/p')
+                    feedback = ""
+                    try:
+                        btn_show_more = self.driver.find_element(By.XPATH,
+                                                                '//*[contains(@class,"expandsummaryicon expand")]').click()  # btn_show_more = driver.find_element(By.XPATH,'//*[@class="expandsummaryicon expand_assignfeedback_comments_386697"]')
+                        comentarios = self.driver.find_elements(By.XPATH,
+                            '//*[contains(@class,"box py-3 boxaligncenter full_assignfeedback_comments")]/p')
 
-                except:
-                    comentarios = self.driver.find_elements_by_xpath(
-                        '//*[contains(@class,"assignfeedback_comments")]/p')
+                    except:
+                        comentarios = self.driver.find_elements(By.XPATH,
+                            '//*[contains(@class,"assignfeedback_comments")]/p')
 
 
-                for comentario in comentarios:
-                    feedback += ' ' + comentario.text
+                    for comentario in comentarios:
+                        feedback += ' ' + comentario.text
 
-                if feedback =="":
-                    feedback = 'Sin comentarios'
-                update_comentarios = "UPDATE actividades SET comentarios ='" + feedback + \
-                                      "' WHERE clave_materia =" + materia + " AND name = '" + activity + "'"
+                    if feedback =="":
+                        feedback = 'Sin comentarios'
+                    update_comentarios = "UPDATE actividades SET comentarios ='" + feedback + \
+                                        "' WHERE clave_materia =" + materia + " AND name = '" + activity + "'"
 
-                cur.execute(update_comentarios)
-                conn.commit()
-                actividad_feedback[activity] = [calificacion, calificado_en, feedback]
+                    cur.execute(update_comentarios)
+                    conn.commit()
+                    actividad_feedback[activity] = [calificacion, calificado_en, feedback]
+                    
+                except Exception as e:
+                    print(e)
+                    pass
                 self.driver.back()
             self.driver.back()
         self.driver.quit()
-
         conn.close()
         return actividad_feedback
 
-#opts = Options()
-#opts.add_argument(
-#             "user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/71.0.3578.80 Chrome/71.0.3578.80 Safari/537.36")
 
-#driver = webdriver.Chrome('C:\Program Files (x86)\chromedriver_win32\chromedriver.exe', options=opts)
-#prueba1 = Feedback(['1255'], ['Unidad 1 / Actividad 5 /','Unidad 2 / Actividad 1 /','Unidad 3 / Actividad 1 /'], driver).extraccion_feedback()
+class tranformData():
+    def __init__(self):pass
 
-
-
-
-
+    def transform_date(date):
+        try:
+            date = date.split(",")
+            date = date[1].split("de")
+            dia = date[0].strip() if len(date[0].strip()) == 2 else "0"+date[0].strip()
+            date = date[2].strip()  + "-" + date[1].strip() + "-" +  dia
+            #reemplazar los nombres de los meses por numeros
+            date = date.replace("January", "01")
+            date = date.replace("February", "02")
+            date = date.replace("March", "03")
+            date = date.replace("April", "04")
+            date = date.replace("May", "05")
+            date = date.replace("June", "06")
+            date = date.replace("July", "07")
+            date = date.replace("August", "08")
+            date = date.replace("September", "09")
+            date = date.replace("October", "10")
+            date = date.replace("November", "11")
+            date = date.replace("December", "12")
+        except:
+            date = None
+        return date
 
 
 
